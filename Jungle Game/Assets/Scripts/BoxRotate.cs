3341ed2660834b0b3ca1f4ea2247ee08462aed43
Hritik Narendra <hritiknarendra@gmail.com>
2018-07-18 22:05:03 +0530

    Box Rotation Script added

---
+++ Jungle Game/Assets/Scripts/BoxRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRotate : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(10, 30, 40), 10);
+    }
+}
